95347758e0c750ab6756bfa12ae782fa308915a7
kdb11 <112870790+kdb11@users.noreply.github.com>
2023-12-28 21:12:17 +0100

    deploytest

---
+++ scripts/deploy.js
@@ -0,0 +1,18 @@
+const hre = require("hardhat");
+
+async function main() {
+  const NAME = "ImagineNFT"
+  const SYMBOL = "IMGNFT"
+  const COST = ethers.utils.parseUnits("1", "ether") // 1 ETH
+
+  const NFT = await hre.ethers.getContractFactory("NFT")
+  const nft = await NFT.deploy(NAME, SYMBOL, COST)
+  await nft.deployed()
+
+  console.log(`Deployed NFT Contract at: ${nft.address}`)
+}
+
+main().catch((error) => {
+  console.error(error);
+  process.exitCode = 1;
+});
