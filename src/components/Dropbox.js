95dd079fac682f0084c70a00b348ca7b58516ebb
kdb11 <112870790+kdb11@users.noreply.github.com>
2024-01-04 09:35:22 +0100

    dropbox onchange

--- src/components/Dropbox.js
+++ src/components/Dropbox.js
@@ -1,17 +1,38 @@
+
 import React from 'react';
+import axios from 'axios';
 
 const Dropbox = () => {
-  return <div className='form'>
-  <form>
-    <input type='text' placeholder='NFT name'></input>
-    <input type='submit' value="Generate NFT"></input>
-  </form>
+/*     state= {
+        selectedFile: null
+    }
 
-  <div className='image'>
-    <img src='' alt="Your NFT"></img>
-  </div>
+    fileSelectedHandler = (e) => {
+        this.state({
+            selectedFile: e.target.files[0]
+        })
+    }
 
-</div>
+    fileUploadHandler = () => {
+        axios.post("")
+    }
+ */
+  return (
+    <div className='form'>
+        <form>
+            <input type='text' placeholder='NFT name'></input>
+            {/* <input type='file' onChange={this.fileSelectedHandler}></input>
+            <button onClick={this.fileUploadHandler}>Upload</button> */}
+            <input type='submit' value="Generate NFT"></input>
+        </form>
+
+        <div className='image'>
+            <img src='' alt="Your NFT"></img>
+        </div>
+
+    </div>
+  )
+  
 };
 
 export default Dropbox;
