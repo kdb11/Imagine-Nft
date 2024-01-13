474847ed8dfb15b4178c2bdd209864cc4d2ece2e
kdb11 <112870790+kdb11@users.noreply.github.com>
2024-01-13 07:46:18 +0100

    dropbox mint

--- src/components/Dropbox.js
+++ src/components/Dropbox.js
@@ -1,16 +1,29 @@
-import React, { useState, useEffect } from 'react';
-import { NFTStorage, File } from 'nft.storage';
+import { useState, useEffect } from 'react';
+import { NFTStorage, File } from 'nft.storage'
 import { ethers } from 'ethers';
+import NFT from '../abis/NFT.json'
+import config from '../config.json';
 
 const Dropbox = () => {
   const [name, setName] = useState("");
   const [url, setURL] = useState(null);
+  const [nft, setNFT] = useState(null);
   const [image, setImage] = useState(null);
   const [provider, setProvider] = useState(null)
 
   const loadBlockchainData = async () => {
     const provider = new ethers.providers.Web3Provider(window.ethereum)
     setProvider(provider)
+
+    const network = await provider.getNetwork()
+
+    const nft = new ethers.Contract(config[network.chainId].nft.address, NFT, provider)
+    setNFT(nft)
+
+    const name = await nft.name()
+    console.log("name", name)
+
+
   }
 
   useEffect(() => {
@@ -38,11 +51,23 @@ const Dropbox = () => {
     }
   };
 
-  const handleSubmit = (e) => {
+  const handleSubmit = async (e) => {
     e.preventDefault();
     
     fileUploadHandler();
+    
+
+    await mintImage(url)
+    console.log("successfull mint")
   };
+
+  const mintImage = async (tokenURI) => {
+    console.log("starting to mint")
+
+    const signer = await provider.getSigner()
+    const transaction = await nft.connect(signer).mint(tokenURI, { value: ethers.utils.parseUnits("1", "ether") })
+    await transaction.wait()
+  }
 
   return (
     <div className='form'>
